3e1d73b9ce966743b76f120f90490ed82abab405
Lxmon <904455569@qq.com>
2022-11-15 07:27:12 +0000

    local vs code use github codespace to modify files

--- FirstPyProject/ngzk_msg_scrip.py
+++ FirstPyProject/ngzk_msg_scrip.py
@@ -131,39 +131,39 @@ def get_batch_msg(access_token, updated_from, member):
         return None
 
 
-def get_one_msg():
-    access_token = get_accsee_token()
-    conn = http.client.HTTPSConnection("api.n46.glastonr.net")
-    payload = ''
-    headers = {
-        'Host': 'api.n46.glastonr.net',
-        'Connection': 'keep-alive',
-        'Accept': '*/*',
-        'X-Talk-App-ID': 'jp.co.sonymusic.communication.nogizaka 2.2',
-        'User-Agent': 'Hot/1.4.01 (jp.co.sonymusic.communication.nogizaka; build:140; iOS 16.0.0) Alamofire/5.6.2',
-        'Accept-Language': 'zh-Hans-CN;q=1.0, en-CN;q=0.9, ja-CN;q=0.8',
-        'Authorization': 'Bearer ' + access_token,
-        'Accept-Encoding': 'br;q=1.0, gzip;q=0.9, deflate;q=0.8',
-        'Content-Type': 'application/json'
-    }
-    conn.request("GET", "/v2/messages/5171", payload, headers)
-    res = conn.getresponse()
-    # 头部信息
-    res_headers = res.headers
-    print(res_headers)
-    if res.getheader("Content-Encoding") is None:
-        # 单获取一条数据时，不会压缩，不需要解压
-        # bytes
-        data = res.read().decode("UTF-8")
-    else:
-        # bytes
-        data = gzip.decompress(res.read()).decode("UTF-8")
-    # 变成字典
-    # dict
-    msg = json.loads(data)
+# def get_one_msg():
+#     access_token = get_accsee_token()
+#     conn = http.client.HTTPSConnection("api.n46.glastonr.net")
+#     payload = ''
+#     headers = {
+#         'Host': 'api.n46.glastonr.net',
+#         'Connection': 'keep-alive',
+#         'Accept': '*/*',
+#         'X-Talk-App-ID': 'jp.co.sonymusic.communication.nogizaka 2.2',
+#         'User-Agent': 'Hot/1.4.01 (jp.co.sonymusic.communication.nogizaka; build:140; iOS 16.0.0) Alamofire/5.6.2',
+#         'Accept-Language': 'zh-Hans-CN;q=1.0, en-CN;q=0.9, ja-CN;q=0.8',
+#         'Authorization': 'Bearer ' + access_token,
+#         'Accept-Encoding': 'br;q=1.0, gzip;q=0.9, deflate;q=0.8',
+#         'Content-Type': 'application/json'
+#     }
+#     conn.request("GET", "/v2/messages/5171", payload, headers)
+#     res = conn.getresponse()
+#     # 头部信息
+#     res_headers = res.headers
+#     print(res_headers)
+#     if res.getheader("Content-Encoding") is None:
+#         # 单获取一条数据时，不会压缩，不需要解压
+#         # bytes
+#         data = res.read().decode("UTF-8")
+#     else:
+#         # bytes
+#         data = gzip.decompress(res.read()).decode("UTF-8")
+#     # 变成字典
+#     # dict
+#     msg = json.loads(data)
 
-    # 顺序输出: sort_keys=True;格式化输出: indent=4; 紧凑输出: separators=(',', ':'); 字符不转义输出: ensure_ascii=False;
-    print(json.dumps(msg, sort_keys=False, indent=4, separators=(',', ':'), ensure_ascii=False))
+#     # 顺序输出: sort_keys=True;格式化输出: indent=4; 紧凑输出: separators=(',', ':'); 字符不转义输出: ensure_ascii=False;
+#     print(json.dumps(msg, sort_keys=False, indent=4, separators=(',', ':'), ensure_ascii=False))
 
 
 def change_time(time):
@@ -172,8 +172,8 @@ def change_time(time):
 
 
 if __name__ == '__main__':
-    member = 'asuka'
-    # 运行前需要把文件清空。（文件连接携带过期参数，所以每次需要重新生成文件。）
+    member = 'renka'
+    # 运行前需要把文件清空。（文件链接携带过期参数，所以每次需要重新生成文件。）
     with open(f'{member}_msg.json', "w", encoding="UTF-8") as f:
         f.truncate(0)
 
